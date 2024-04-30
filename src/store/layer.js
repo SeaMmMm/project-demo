99e903f727452f98897f1a0bc1cdba91056d9d23
Seaming <347552878@qq.com>
2024-04-30 18:02:48 +0800

    添加遮罩

---
+++ src/store/layer.js
@@ -0,0 +1,10 @@
+import { create } from 'zustand'
+
+const useLayer = create((set) => ({
+  isShowLayer: false,
+  toggleLayer: () => set((state) => ({ isShowLayer: !state.isShowLayer })),
+  setLayerToFalse: () => set({ isShowLayer: false }),
+  setLayerToTrue: () => set({ isShowLayer: true }),
+}))
+
+export default useLayer
