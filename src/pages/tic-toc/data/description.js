e2b035ae0d02563b185eadfb9d0cc31e34922d3f
Seaming <347552878@qq.com>
2024-07-23 11:15:48 +0800

    chore: Update codeurl in Tic-Toc game description

--- src/pages/tic-toc/data/description.js
+++ src/pages/tic-toc/data/description.js
@@ -1,6 +1,7 @@
 const info = {
   description: 'Tic-Toc game',
-  codeurl: '',
+  codeurl:
+    'https://github.com/SeaMmMm/project-demo/blob/main/src/pages/tic-toc/TicToc.jsx',
 }
 
 export default info
